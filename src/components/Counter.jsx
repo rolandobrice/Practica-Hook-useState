8c4164d00b9d29f8a192cbce00d51bbf487bc898
rolandobrice <rolandojose1109@gamil.com>
2024-04-28 19:15:01 -0400

    add: un contador en componente Couter con useState

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counter = () => {
+
+    let [obj, setOjb] = useState({num:1})
+
+    const [count, setCount] = useState(0)
+
+    const handleClick = () => {
+        const newObj = {... obj, num: obj.num + 1}
+        setOjb(newObj)
+    }
+
+    const incrementaX3 = () => {
+        setCount(count => count +1)
+        setCount(count => count +1)
+        setCount(count => count +1)
+
+    }
+
     return (
-        <div></div>
+        <div>
+            <h2>Counter</h2>
+            <p>Contador: {obj.num}</p>
+            <button onClick={handleClick}>incrementa</button>
+
+            <p>Contador: 2 {count}</p>
+            <button onClick={incrementaX3}>incrementa x 3</button>
+        </div>
     )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,16 +4,28 @@ const Home = () => {
 
     const [mensaje, setMensaje] = useState('Hola Mundo')
     const [numero, setNumero] = useState(3)
+    const [nombre, setNombre] = useState(['Luis'])
 
     const handleClick = () => {
         setNumero(5)
     }
+
+    const addName = () => {
+        setNombre([...nombre,'Rolando'])
+    }
+
     return (
         <div>
             <h2>useState Example</h2>
             <p>{mensaje}</p>
             <p>{numero}</p>
             <button onClick={handleClick}>Pulsa</button>
+            <button onClick={addName}>Agregar Nombre</button>
+            <ul>
+                {nombre.map (nombre => (
+                    <li>{nombre}</li>
+                ))}
+            </ul>
         </div>
     )
 }
